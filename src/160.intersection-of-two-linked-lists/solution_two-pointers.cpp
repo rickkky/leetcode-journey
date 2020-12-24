4b074ed1c04077f4c9664cbb67d3cb62eb9a3771
ricky <rickkkkkkkkky@foxmail.com>
2020-12-24 16:38:55 +0800

    feat(p160, ts, cpp): two pointers solution

---
+++ src/160.intersection-of-two-linked-lists/solution_two-pointers.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=cpp
+ *
+ * [160] 相交链表
+ */
+
+struct ListNode {
+  int val;
+  ListNode *next;
+};
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *   int val;
+ *   ListNode *next;
+ *   ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+ public:
+  ListNode *getIntersectionNode(ListNode *l1, ListNode *l2) {
+    if (!l1 || !l2) {
+      return nullptr;
+    }
+
+    ListNode *p1 = l1;
+    ListNode *p2 = l2;
+
+    while (p1 != p2) {
+      p1 = p1 ? p1->next : l2;
+      p2 = p2 ? p2->next : l1;
+    }
+
+    return p1;
+  }
+};
+// @lc code=end
---
+++ src/160.intersection-of-two-linked-lists/solution_two-pointers.ts
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=typescript
+ *
+ * [160] 相交链表
+ */
+
+interface ListNode {
+  val: number;
+  next: ListNode | null;
+}
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function getIntersectionNode(
+  l1: ListNode | null,
+  l2: ListNode | null,
+): ListNode | null {
+  if (!l1 || !l2) {
+    return null;
+  }
+
+  let p1: ListNode | null = l1;
+  let p2: ListNode | null = l2;
+
+  while (p1 !== p2) {
+    p1 = p1 ? p1.next : l2;
+    p2 = p2 ? p2.next : l1;
+  }
+
+  return p1;
+}
+// @lc code=end
+
+export default getIntersectionNode;
