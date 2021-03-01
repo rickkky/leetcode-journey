af6ebec5111cc7d9d7f96eba10f36d8dcd9cdad5
ricky <rickkkkkkkkky@foxmail.com>
2021-03-01 16:12:29 +0800

    feat(p150, ts): aux stack solution

---
+++ src/150.evaluate-reverse-polish-notation/solution_aux-stack.ts
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=150 lang=typescript
+ *
+ * https://leetcode-cn.com/problems/evaluate-reverse-polish-notation/
+ */
+
+// @lc code=start
+
+// the given RPN expression is always valid
+function evalRPN(tokens: string[]): number {
+  // a stack to store numbers
+  const nums: number[] = [];
+
+  for (let token of tokens) {
+    if (token === '+' || token === '-' || token === '*' || token === '/') {
+      const num1 = nums.pop() as number;
+      const num2 = nums.pop() as number;
+
+      if (token === '+') {
+        nums.push(num2 + num1);
+      } else if (token === '-') {
+        nums.push(num2 - num1);
+      } else if (token === '*') {
+        nums.push(num2 * num1);
+      } else if (token === '/') {
+        nums.push(parseInt((num2 / num1).toFixed(0)));
+      }
+    } else {
+      nums.push(parseInt(token));
+    }
+  }
+
+  return nums.pop() as number;
+}
+
+// @lc code=end
+
+export default evalRPN;
