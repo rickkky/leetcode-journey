9364cb83831348b8e1de2e2ba44d78cfb3435c17
ricky <rickkkkkkkkky@foxmail.com>
2021-04-04 21:10:40 +0800

    refactor(p494): optimize code readability and add comments

--- src/494.target-sum/solution_dynamic-programming.ts
+++ src/494.target-sum/solution_dynamic-programming.ts
@@ -6,28 +6,37 @@
 
 // @lc code=start
 
-function findTargetSumWays(nums: number[], sum: number): number {
-  // indicates that we have dp[n] ways to get n
-  let dp = new Array(2001).fill(0);
+// `nums` is not empty and it's elements are non-negative
+// the sum of elements in `nums` will not exceed 1000
+function findTargetSumWays(nums: number[], target: number): number {
+  // indicates that we have `schemes[sum + 1000]` ways to get `sum`
+  let schemes = new Array(2001).fill(0);
 
-  dp[nums[0] + 1000] += 1;
-  dp[-nums[0] + 1000] += 1;
+  schemes[nums[0] + 1000] += 1;
+  schemes[-nums[0] + 1000] += 1;
 
   for (let i = 1; i < nums.length; ++i) {
-    const next = new Array(2001).fill(0);
+    const nextSchemes = new Array(2001).fill(0);
 
-    for (let s = -1000; s <= 1000; ++s) {
-      if (dp[s + 1000] > 0) {
-        next[nums[i] + s + 1000] += dp[s + 1000];
-        next[-nums[i] + s + 1000] += dp[s + 1000];
+    for (let sum = -1000; sum <= 1000; ++sum) {
+      if (nums[i] + sum + 1000 <= 2000) {
+        nextSchemes[nums[i] + sum + 1000] += schemes[sum + 1000];
+      }
+      if (-nums[i] + sum + 1000 >= 0) {
+        nextSchemes[-nums[i] + sum + 1000] += schemes[sum + 1000];
       }
     }
 
-    dp = next;
+    schemes = nextSchemes;
   }
 
-  return sum > 1000 ? 0 : dp[sum + 1000];
+  return Math.abs(target) > 1000 ? 0 : schemes[target + 1000];
 }
+
+// time: O(sum * n).
+// - sum: the maximum sum of elements in `nums`.
+
+// space: O(sum).
 
 // @lc code=end
 
